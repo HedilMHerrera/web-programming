3e7788509a570cd6288837c94c7e6b07b517d11f
Hedi <montanohedilberto@gmail.com>
2025-11-21 12:02:04 -0400

    refactor: Main routes

--- Backend/src/routes/quest.js
+++ Backend/src/routes/quest.js
@@ -2,11 +2,18 @@ const express = require('express');
 const router = express.Router();
 
 const difficultyCrtl = require('../controllers/difficultyController');
+const ageRangeCrtl = require('../controllers/ageRangeController');
 
 router.get('/difficulties', difficultyCrtl.list);
 router.get('/difficulties/:id', difficultyCrtl.get);
 router.post('/difficulties', difficultyCrtl.create);
 router.put('/difficulties/:id', difficultyCrtl.update);
 router.delete('/difficulties/:id', difficultyCrtl.remove);
+
+router.get('/ageranges', ageRangeCrtl.list);
+router.get('/ageranges/:id', ageRangeCrtl.get);
+router.post('/ageranges', ageRangeCrtl.create);
+router.put('ageranges/:id', ageRangeCrtl.update);
+router.delete('/ageranges/:id', ageRangeCrtl.remove);
 
 module.exports = router;
