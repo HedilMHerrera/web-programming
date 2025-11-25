f7a872e4cf3b1b294074e7b608afb691163e7284
alejandro <alejandrotorrico743@gmail.com>
2025-11-25 01:15:12 -0400

    add:routes for the categories

--- Backend/src/routes/quest.js
+++ Backend/src/routes/quest.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const difficultyCrtl = require('../controllers/difficultyController');
 const ageRangeCrtl = require('../controllers/ageRangeController');
+const categoryCrtl = require('../controllers/categoryController');
 
 router.get('/difficulties', difficultyCrtl.list);
 router.get('/difficulties/:id', difficultyCrtl.get);
@@ -15,5 +16,11 @@ router.get('/ageranges/:id', ageRangeCrtl.get);
 router.post('/ageranges', ageRangeCrtl.create);
 router.put('/ageranges/:id', ageRangeCrtl.update);
 router.delete('/ageranges/:id', ageRangeCrtl.remove);
+
+router.get('/categories', categoryCrtl.list);
+router.get('/categories/:id', categoryCrtl.get);
+router.post('/categories', categoryCrtl.create);
+router.put('/categories/:id', categoryCrtl.update);
+router.delete('/categories/:id', categoryCrtl.remove);
 
 module.exports = router;
