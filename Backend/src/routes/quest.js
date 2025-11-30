a5dbb342dba262c4f83019f848311b9062c3ef7d
alejandro <alejandrotorrico743@gmail.com>
2025-11-29 23:03:19 -0400

    feat: add generic list query validator for pagination and filters

--- Backend/src/routes/quest.js
+++ Backend/src/routes/quest.js
@@ -8,6 +8,7 @@ const subcategoryCtrl = require('../controllers/subcategoryController');
 const validate = require('../middlewares/validate');
 const difficultyValidator = require('../middlewares/validators/difficultyValidator');
 const ageRangeValidator = require('../middlewares/validators/ageRangeValidator');
+const listQueryValidator = require('../middlewares/validators/listQueryValidator'); 
 
 router.get('/', (req, res) => {
     res.json({
@@ -16,25 +17,25 @@ router.get('/', (req, res) => {
     });
 });
 
-router.get('/difficulties', difficultyCrtl.list);
+router.get('/difficulties', listQueryValidator, validate, difficultyCrtl.list);
 router.get('/difficulties/:id', difficultyValidator.getById, validate, difficultyCrtl.get);
 router.post('/difficulties', difficultyValidator.create, validate, difficultyCrtl.create);
 router.put('/difficulties/:id', difficultyValidator.update, validate, difficultyCrtl.update);
 router.delete('/difficulties/:id', difficultyValidator.delete, validate, difficultyCrtl.remove);
 
-router.get('/ageranges', ageRangeCrtl.list);
+router.get('/ageranges',listQueryValidator, validate, ageRangeCrtl.list);
 router.get('/ageranges/:id', ageRangeValidator.getById, validate, ageRangeCrtl.get);
 router.post('/ageranges', ageRangeValidator.create, validate, ageRangeCrtl.create);
 router.put('/ageranges/:id', ageRangeValidator.update, validate, ageRangeCrtl.update);
 router.delete('/ageranges/:id', ageRangeValidator.delete, validate, ageRangeCrtl.remove);
 
-router.get('/categories', categoryCrtl.list);
+router.get('/categories', listQueryValidator, validate, categoryCrtl.list);
 router.get('/categories/:id', categoryCrtl.get);
 router.post('/categories', categoryCrtl.create);
 router.put('/categories/:id', categoryCrtl.update);
 router.delete('/categories/:id', categoryCrtl.remove);
 
-router.get('/subcategories', subcategoryCtrl.list);
+router.get('/subcategories', listQueryValidator, validate, subcategoryCtrl.list);
 router.get('/subcategories/:id', subcategoryCtrl.get);
 router.post('/subcategories', subcategoryCtrl.create);
 router.put('/subcategories/:id', subcategoryCtrl.update);
